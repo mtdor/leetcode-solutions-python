2e07be1401c4dcaf681b7b0e8ec59f7826d8c21e
sergey.g <sergey.golota.dev@gmail.com>
2022-08-17 19:53:01 +0300

    2. Add Two Numbers

--- leetcode/2_Add_Two_Numbers.py
+++ leetcode/2_Add_Two_Numbers.py
@@ -12,35 +12,36 @@ class ListNode:
 
     def __repr__(self):
         return f'{self.val}, {self.next}'
-        # return f'{self.val}'
 
 
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        result_tree: ListNode = ListNode()
-        curr_node = result_tree
+        root_node: ListNode = ListNode()
+        curr_node = root_node
         carry = 0
-        while l1 or l2:
-            nodes_sum = l1.val + l2.val + carry
-            curr_node.val = nodes_sum % 10
+        while l1 or l2 or carry:
+            l1_val = l1.val if l1 else 0
+            l2_val = l2.val if l2 else 0
+            nodes_sum = l1_val + l2_val + carry
+            val = nodes_sum % 10
             carry = nodes_sum // 10
-            l1, l2 = l1.next, l2.next
-            new_node = ListNode()
+            new_node = ListNode(val)
             curr_node.next = new_node
             curr_node = new_node
-            # result = l1_val
-        return result_tree
+            l1 = l1.next if l1 else None
+            l2 = l2.next if l2 else None
+        return root_node.next
 
 
 if __name__ == '__main__':
     print(ListNode())
     # list1 = ListNode(2, ListNode(4, ListNode(3)))
-    list1 = ListNode(5, ListNode(6))
+    list1 = ListNode(6, ListNode(8))
     # list1 = ListNode(2)
     print('l1', list1)
     # list2 = ListNode(5, ListNode(6, ListNode(4)))
+    list2 = ListNode(4, ListNode(1))
     # list2 = ListNode(5)
-    list2 = ListNode(9, ListNode(2))
     print('l2', list2)
     list3: ListNode = Solution().addTwoNumbers(list1, list2)
     print('l3', list3)
